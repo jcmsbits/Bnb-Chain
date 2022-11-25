ec44b560291f5e1cc4668af0f1a71c3abd66e6fb
Jose Carlos <105695892+Carameloazul@users.noreply.github.com>
2022-11-25 11:26:17 -0600

    Delete HelloWorld.sol

--- HelloWorld.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity  >=0.8.0 <0.9;
-contract HelloWorld {
-    function sayHelloWorld() public pure returns (string memory) {
-        return "Hello World";
-    }
-}
